7f0e6b9e74eeb419c80a2c75aca2cd1238dc44c9
Greis <mort5240@gmail.com>
2017-02-23 14:01:26 +0100

    MainPage is under construction. Our song entiry/model is made with correct properties

--- Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
+++ Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mobile_Cust_LstViews.Models;
 using Xamarin.Forms;
 
 namespace Mobile_Cust_LstViews
 {
     public partial class MainPage : ContentPage
     {
+        private List<Song> _songs = new List<Song>();
+        private CustomListView _songListView = new CustomListView(ListViewCachingStrategy.RetainElement, _songs);
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+
+    }
+
+    class CustomListView : ListView
+    {
+
+        public CustomListView(ListViewCachingStrategy strategy, List<Song> songs) : base(strategy)
+        {
+            
+        }
+
+        protected override void SetupContent(Cell content, int index)
+        {
+            base.SetupContent(content, index);
+        }
+    }
+
+    class SongCell : ViewCell
+    {
+        
     }
 }
---
+++ Mobile_Cust_LstViews/Mobile_Cust_LstViews/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Cust_LstViews.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int YearReleased { get; set; }
+        public int BPM { get; set; }
+        public bool IsFavorite { get; set; }
+
+    }
+}
