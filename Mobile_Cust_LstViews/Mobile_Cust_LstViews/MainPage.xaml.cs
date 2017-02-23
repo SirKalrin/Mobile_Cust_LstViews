7fdf5743321702a6fb7a21386e4283adefd067c8
Greis <mort5240@gmail.com>
2017-02-23 14:06:42 +0100

    Woops forgot to save the file before adding/committing/pushing -.-

--- Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
+++ Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
@@ -10,12 +10,14 @@ namespace Mobile_Cust_LstViews
 {
     public partial class MainPage : ContentPage
     {
-        private List<Song> _songs = new List<Song>();
-        private CustomListView _songListView = new CustomListView(ListViewCachingStrategy.RetainElement, _songs);
+        private List<Song> _songs;
+        private CustomListView _songListView;
 
         public MainPage()
         {
             InitializeComponent();
+            _songs = new List<Song>();
+            _songListView = new CustomListView(ListViewCachingStrategy.RetainElement, _songs);
         }
 
 
@@ -37,6 +39,30 @@ namespace Mobile_Cust_LstViews
 
     class SongCell : ViewCell
     {
-        
+        private StackLayout _cellLayout;
+        private StackLayout _desciptionStack;
+        private StackLayout _metaData1;
+        private StackLayout _metaData2;
+
+        private Image _isFavorite;
+
+
+        public SongCell()
+        {
+            //Initializing View Objects
+            _cellLayout = new StackLayout();
+            _desciptionStack = new StackLayout();
+            _metaData1 = new StackLayout();
+            _metaData2 = new StackLayout();
+
+
+
+            //Setting Layout Orientation
+            _cellLayout.Orientation = StackOrientation.Horizontal;
+            _desciptionStack.Orientation = StackOrientation.Horizontal;
+            _metaData1.Orientation = StackOrientation.Vertical;
+            _metaData2.Orientation = StackOrientation.Vertical;
+        }
+
     }
 }
