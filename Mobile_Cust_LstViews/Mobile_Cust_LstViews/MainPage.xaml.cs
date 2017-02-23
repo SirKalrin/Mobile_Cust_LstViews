46f4e74260d12feb19ffe53f79d7c00c27e39156
BilleTheBug <dr.iversen@hotmail.com>
2017-02-23 14:26:00 +0100

    labels created, bindings created, layout organized, dummydata on its way.

--- Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
+++ Mobile_Cust_LstViews/Mobile_Cust_LstViews/MainPage.xaml.cs
@@ -15,12 +15,19 @@ namespace Mobile_Cust_LstViews
 
         public MainPage()
         {
-            InitializeComponent();
-            _songs = new List<Song>();
+            //InitializeComponent();
+            InitializeDummyData();
             _songListView = new CustomListView(ListViewCachingStrategy.RetainElement, _songs);
         }
 
-
+        private void InitializeDummyData()
+        {
+            _songs = new List<Song>()
+            {
+                new Song() {Artist = "Red warszawa", Album = "Heavy metal og hash", Title = "Julemandens selvmordsbrev", BPM = 120, IsFavorite = true,YearReleased = 1994},
+                new Song() {Artist = "Red warszawa", Album = "Heavy metal og hash", Title = "Dødshimlen", BPM = 140, IsFavorite = false,YearReleased = 1994}
+            };
+        }
     }
 
     class CustomListView : ListView
@@ -45,7 +52,12 @@ namespace Mobile_Cust_LstViews
         private StackLayout _metaData2;
 
         private Image _isFavorite;
-
+        Label _title;
+        Label _artist;
+        Label _album;
+        Label _yearReleased;
+        Label _bpm;
+        private const string favoriteURL = "../images";
 
         public SongCell()
         {
@@ -55,13 +67,38 @@ namespace Mobile_Cust_LstViews
             _metaData1 = new StackLayout();
             _metaData2 = new StackLayout();
 
+            _title = new Label();
+            _artist = new Label();
+            _album = new Label();
+            _yearReleased = new Label();
+            _bpm = new Label();
+            _isFavorite = new Image();
 
+            _title.SetBinding(Label.TextProperty, "Title");
+            _artist.SetBinding(Label.TextProperty, "Artist");
+            _album.SetBinding(Label.TextProperty, "Album");
+            _yearReleased.SetBinding(Label.TextProperty, "YearReleased");
+            _bpm.SetBinding(Label.TextProperty, "BPM");
 
             //Setting Layout Orientation
             _cellLayout.Orientation = StackOrientation.Horizontal;
             _desciptionStack.Orientation = StackOrientation.Horizontal;
             _metaData1.Orientation = StackOrientation.Vertical;
             _metaData2.Orientation = StackOrientation.Vertical;
+
+            _desciptionStack.Children.Add(_metaData1);
+            _desciptionStack.Children.Add(_metaData2);
+            _cellLayout.Children.Add(_desciptionStack);
+            _cellLayout.Children.Add(_isFavorite);
+            
+            _metaData1.Children.Add(_title);
+            _metaData1.Children.Add(_artist);
+            _metaData1.Children.Add(_album);
+            _metaData2.Children.Add(_yearReleased);
+            _metaData2.Children.Add(_bpm);
+
+
+
         }
 
     }
